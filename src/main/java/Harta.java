534b213494dfb23ca0c17af7e74ad7533689f719
rosca_ioana <rosca.ioana11@gmail.com>
2021-03-07 15:34:01 +0200

    Updated Map

--- src/main/java/Harta.java
+++ src/main/java/Harta.java
@@ -12,19 +12,83 @@ public class Harta {
 
         while(true){
             displayMap();
-            System.out.println("Pick an option: " + "\n" + "1. Change a tile" + "\n" + "2. Reset the map" + "\n" + "0. Exit");
+            System.out.println("Pick an option: " + "\n" + "1. Change a tile" + "\n" + "2. Reset the map" + "\n"
+                    + "3. Change Rectangle Tile Area" + "\n" + "4. Change Circle Tile Area" + "\n" + "0. Exit");
 
             String pick = read.nextLine();
 
-
             if(pick.equals("1")){
-                int playerX = read.nextInt();
-                int playerY = read.nextInt();
+                int playerX = -1;
+                while(playerX == -1){
+                    System.out.println("x = ");
+                    playerX = readCoordinateX(read);
+                }
+                int playerY = -1;
+                while (playerY == -1){
+                    System.out.println("y = ");
+                    playerY = readCoordinateY(read);
+                }
                 read.nextLine();
-                String drawing = read.nextLine();
+                String drawing = "";
+                while (drawing.equals("")){
+                    System.out.println("Pick '^' = mountain, '~' = water, ' ' = 'field'");
+                    drawing = readTile(read);
+                }
                 setTile(playerX,playerY,drawing);
-            }else if(pick.equals("2")){
+            }else if(pick.equals("2")) {
                 resetMap();
+            }else if(pick.equals("3")){
+                int playerX1 = -1;
+                while(playerX1 == -1){
+                    System.out.println("x1 = ");
+                    playerX1 = readCoordinateX(read);
+                }
+                int playerY1 = -1;
+                while (playerY1 == -1){
+                    System.out.println("y1 = ");
+                    playerY1 = readCoordinateY(read);
+                }
+                int playerX2 = -1;
+                while (playerX2 == -1){
+                    System.out.println("x2 = ");
+                    playerX2 = readCoordinateX(read);
+                }
+                int playerY2 = -1;
+                while (playerY2 == -1){
+                    System.out.println("y2 = ");
+                    playerY2 = readCoordinateY(read);
+                }
+                read.nextLine();
+                String drawing = "";
+                while(drawing.equals("")){
+                    System.out.println("tile = ");
+                    System.out.println("Pick '^' = mountain, '~' = water, ' ' = 'field'");
+                    drawing = readTile(read);
+                }
+                if(drawing.length() == 1 && playerX1 != -1 && playerY1 != -1
+                        && playerX2 != -1 && playerY2 != -1){ //verifica daca este in range-ul corect
+                    setRectangle(playerX1, playerY1, playerX2, playerY2, drawing);
+                }
+            }else if(pick.equals("4")){
+                int playerX = -1;
+                while(playerX == -1){
+                    System.out.println("x = ");
+                    playerX = readCoordinateX(read);
+                }
+                int playerY = -1;
+                while (playerY == -1){
+                    System.out.println("y = ");
+                    playerY = readCoordinateY(read);
+                }
+                int radius = read.nextInt();
+
+                read.nextLine();
+                String drawing = "";
+                while (drawing.equals("")){
+                    System.out.println("Pick '^' = mountain, '~' = water, ' ' = 'field'");
+                    drawing = readTile(read);
+                }
+                setCircle(playerX, playerY, radius , drawing);
             }else if(pick.equals("0")){
                 break;
             }else{
@@ -32,6 +96,7 @@ public class Harta {
             }
         }
         displayMap();
+        //TODO Refactor main
     }
 
     public static void resetMap(){
@@ -53,7 +118,65 @@ public class Harta {
     }
 
     public static void setTile(int coordonataX, int coordonataY, String tile){
-        worldMap[coordonataX][coordonataY] = tile;
+        worldMap[coordonataY][coordonataX] = tile;
+    }
 
+    public static void setRectangle(int coordonataX1, int coordonataY1, int coordonataX2, int coordonataY2, String tile){
+        for(int i = coordonataY1; i <= coordonataY2; i++){
+            for (int j = coordonataX1; j <= coordonataX2; j++){
+                setTile(j, i, tile);
+            }
+        }
+    }
+
+    public static String readTile(Scanner read){
+        String scan = read.nextLine();
+        if(scan.equals("^")){
+            return scan;
+        }else if(scan.equals("~")){
+            return scan;
+        }else if(scan.equals(" ")){
+            return scan;
+        }else{
+            System.out.println("Not the right choice (^, ~, ' ')");
+            return "";
+        }
+    }
+
+    public static int readCoordinateX(Scanner read){
+        int scan = read.nextInt();
+        if(scan >= 0 && scan < mapWidth){
+            return scan;
+        }else{
+            System.out.println("Coordinate X Out of Range");
+            return -1;
+        }
+    }
+
+    public static int readCoordinateY(Scanner read){
+        int scan = read.nextInt();
+        if(scan >= 0 && scan < mapHeight){
+            return scan;
+        }else {
+            System.out.println("Coordinate Y Out of Range");
+            return -1;
+        }
+    }
+
+    public static void setCircle(int oX, int oY, int oR, String tile){
+        for(int i = oY - oR + 1; i < oY + oR; i++){
+            for(int j = oX - oR + 1; j < oX + oR; j++){
+//                double m = Math.atan2(oY - i, oX - j);
+//                double pointX = oR * Math.cos(m);
+//                double pointY = oR * Math.sin(m);
+                double deltaX = j - oX;
+                double deltaY = i - oY;
+                double squareDistance = deltaX * deltaX + deltaY * deltaY;
+                //if(oR * oR >= (oR * Math.cos(m) - oX) * (oR * Math.cos(m) - oX) + (oR * Math.sin(m) - oY) * (oR * Math.sin(m) - oY)){
+                if(oR * oR >= squareDistance){
+                    setTile(j, i, tile);
+                }
+            }
+        }
     }
 }
